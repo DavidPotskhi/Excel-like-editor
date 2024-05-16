2ce0430f0f6d698fd0fc0c025315ca131a034396
David Potskhishvili <tallerbog225@gmail.com>
2024-05-16 13:56:14 +0200

    Added some helper functions for tokenizer

--- src/Tokenizer.kt
+++ src/Tokenizer.kt
@@ -9,20 +9,74 @@ class CloseBracket : Token()
 data class Function(val name: String) : Token()
 data class CellReference(val cellReference: String) : Token()
 class QuoteToken : Token()
+data class BinOperand(val operand: String) : Token()
 
-class Tokenizer(val inputFormula: String, var iterator: CharIterator) {
 
-    constructor(inputFormula: String) : this(inputFormula, inputFormula.iterator())
+class PeekingIterator(private val iterator: CharIterator){
+    private var hasNextPeeked: Boolean = false
+    private var peekedElement: Char? = null
+
+    fun hasNext(): Boolean {
+        return hasNextPeeked || iterator.hasNext()
+    }
+
+    fun next(): Char {
+        if (!hasNextPeeked) {
+            return iterator.next()
+        }
+        val result = peekedElement
+        hasNextPeeked = false
+        peekedElement = null
+        return result!!
+    }
+
+    fun peek() : Char {
+        if (!hasNextPeeked) {
+            hasNextPeeked = true
+            peekedElement = iterator.next()
+        }
+        return peekedElement!!
+    }
+}
+
+
+
+
+class Tokenizer(private var inputFormula: String) {
+
+    private var iterator: PeekingIterator = PeekingIterator(this.inputFormula.iterator())
+    private var currentToken: Token?
+
+    init {
+        this.currentToken = next()
+    }
 
     fun isEnd() : Boolean {
-        return !iterator.hasNext();
+        return !iterator.hasNext()
     }
 
-    fun next() {
+    fun next() : Token? {
+        if (!iterator.hasNext()) {
+            return null;
+        }
 
+        while (iterator.hasNext() && (iterator.peek().isWhitespace() || iterator.peek() == '\n'
+                    || iterator.peek() == '\t' || iterator.peek() == '\r')) {
+            iterator.next();
+        }
+
+        when (iterator.peek()) {
+            //else -> throw
+        }
+
+        while (iterator.hasNext() && (iterator.peek().isWhitespace() || iterator.peek() == '\n'
+                    || iterator.peek() == '\t' || iterator.peek() == '\r')) {
+            iterator.next();
+        }
+        return currentToken;
     }
 
-    fun getToken() : Token {
-        return OpenBracket()
+    fun getToken() : Token? {
+        return currentToken
     }
 }
