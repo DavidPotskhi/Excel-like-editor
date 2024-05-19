1cf09b74c65150c52959955762a88014bb1035a1
David Potskhishvili <tallerbog225@gmail.com>
2024-05-19 14:54:17 +0200

    Little refactoring

--- src/Interpreter.kt
+++ src/Interpreter.kt
@@ -4,10 +4,36 @@ import kotlin.math.max
 class Interpreter {
 
     val functions = hashMapOf<String, (MutableList<Int>) -> Unit>(
-        "+" to { it.add(it.removeLast() + it.removeLast()) },
-        "*" to { it.add(it.removeLast() * it.removeLast()) },
-        "-" to { it.add(-it.removeLast() + it.removeLast()) },
-        "max" to { it.add(max(it.removeLast(), it.removeLast())) },
+        "+" to {
+            if (it.size < 2) {
+                throw InterpreterException("+ operator should have exactly two arguments")
+            }
+            it.add(it.removeLast() + it.removeLast())
+        },
+        "*" to {
+            if (it.size < 2) {
+                throw InterpreterException("* operator should have exactly two arguments")
+            }
+            it.add(it.removeLast() * it.removeLast())
+        },
+        "-" to {
+            if (it.size < 2) {
+                throw InterpreterException("- operator should have exactly two arguments")
+            }
+            it.add(-it.removeLast() + it.removeLast())
+        },
+        "max" to {
+            if (it.size < 2) {
+                throw InterpreterException("max function should have exactly two arguments")
+            }
+            it.add(max(it.removeLast(), it.removeLast()))
+        },
+        "inv" to {
+            if (it.size < 1) {
+                throw InterpreterException("+ operator should have exactly two arguments")
+            }
+            it.add(it.removeLast().inv())
+        }
     )
 
     fun eval(instruction: Instruction, stack: MutableList<Int>) {
@@ -15,11 +41,16 @@ class Interpreter {
             is Number -> stack.add(instruction.number)
             is CellReference -> {}
             is Function -> {
+                if (!functions.containsKey(instruction.functionName)) {
+                    throw InterpreterException("Interpreter doesn't have function called: $instruction")
+                }
                 functions[instruction.functionName]!!.invoke(stack)
             }
+
             else -> throw InterpreterException("Illegal instruction in Interpreter: $instruction")
         }
     }
+
     fun executeInstructions(instructions: Queue<Instruction>): Int {
         val solveStack = mutableListOf<Int>()
         for (instruction in instructions) {
@@ -27,7 +58,6 @@ class Interpreter {
         }
         return solveStack.removeLast()
     }
-
 
 
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val s = "1 + 2 * 3 - 4 + -max(1, max(1, max(1,2)))"
+    val s = "(max(1,2) + max(-1, 3)) * (-max(max(1,2), max(3,4)))"
     val tokens = Tokenizer(s).tokenize()
     val parser = Parser(tokens)
     parser.expressionRead()
